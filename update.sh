cab151d15720fae8a8854294ef67fe8e2f05e3cc
Rainer Jung <r@iner.cc>
2022-03-14 19:22:49 +0100

    Cleanup

--- update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-./update.rb --arch mips64,armv7,s390x,armhf,ppc64le,aarch64,x86,x86_64 -r main,community,testing -v edge,latest-stable
-git add -p .
-
-export DOCKER_CLI_EXPERIMENTAL=enabled
